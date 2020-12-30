619bfa8a7ddb449863445d55d7251230e376b016
elenagiura <elenagiura94@gmail.com>
2020-12-30 09:11:31 +0100

    Variable changes

--- js/homework.js
+++ js/homework.js
@@ -5,15 +5,15 @@ Peter has 3500$, and Jim has 5000$, print if Peter has more money than Jim (cons
 var Peter = 3500;
 var Jim = 5000;
 
-console.log("If Peter has more money than Jim?")
-console.log(Peter > Jim ? true + " -Peter has more money" : false +" -Peter has less money")
+console.log("If Peter has more money than Jim?");
+console.log(Peter > Jim ? "Peter has more money" : "Peter has less money");
 
 /* TASK 2: 
 John is 38 years old. In 8 years John will be two times older than Mike. How old is Mike now (console log result)
 */
 
 var John = 38;
-var Mike = (38 + 8)/2 - 8;
+var Mike = (John + 8)/2 - 8;
 
 console.log ("Mike is "+ Mike +" years old")
 
@@ -23,8 +23,11 @@ There are 78 birds in a first flock, and 57 birds in a second flock.
 Is the first flock larger or equal to the second one ? (console log result, true or false)
 */
 
-var firstFlock = 78 - 12;
-var secondFlock = 57 + 12 - 4;
+var firstFlock = 78;
+var secondFlock = 57;
+
+firstFlock= firstFlock-12;
+secondFlock= secondFlock+12-4;
 
 console.log("Is the first flock larger or equal to the second one ?")
-console.log(firstFlock >= secondFlock ? true + " -First flock is larger" : false + " -Second flock is larger")
+console.log(firstFlock >= secondFlock ? "First flock is larger" : "Second flock is larger")
