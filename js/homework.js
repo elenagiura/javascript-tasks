a5f4d7edcf011ff0ce3e1c6b670c38172c442128
elenagiura <elenagiura94@gmail.com>
2021-02-03 13:25:20 +0100

    Constructor functions

--- js/homework.js
+++ js/homework.js
@@ -1,282 +1,174 @@
-/* ***** JS PRACTISING *****
+/* TASK 1:
 
-TASK 1:
-
-Based on object:
-
-var someData = {
-  name: "Peter",
-  lastName: "Dinklage",
-  status: "married"
-};
-
-Create a function that should receive this object,
-and repack it to the new object where values from previous object represent both,
- keys and values of the new object. */
+Create a constructor function for generating student objects. 
+Each student object should have name, last name, age and average grade. */
 
 console.log("TASK 1:");
 
-var someData = {
-  name: "Peter",
-  lastName: "Dinklage",
-  status: "married"
-};
-
-function repackTaskOne (person) {
-  var repackPerson = new Object();
-  repackPerson[person.name] = person.name;
-  repackPerson[person.lastName] = person.lastName;
-  repackPerson[person.status] = person.status;
-
-  return repackPerson;
+function Student(firstName, lastName, age, averageGrade) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.averageGrade = averageGrade;
 }
 
-console.log(repackTaskOne(someData));
+var marko = new Student("Marko", "Milosevic", 26, 9.50);
 
-/* TASK 2: 
+console.log(marko);
+console.log(marko.__proto__)
 
-Based on array:
+/* TASK 2:
 
-var someData = [13, 45, 56, [32, 11], 27, [55, 92]];
-
-Create a function that should repack this array to another one where all numbers are contained in the same array,
-in the exact order like in provided array. */
+Create a constructor function for generating objects with data about players of your favorite football team 
+(name, age, goals scored, yellow cards). You can find statistics online. */
 
 console.log("TASK 2:");
 
-someData = [13, 45, 56, [32, 11], 27, [55, 92]];
+function FootballPlayer(name, age, goals, yellowCards) {
+  this.name = name;
+  this.age = age;
+  this.goals = goals;
+  this.yellowCards = yellowCards;
+}
 
- function repackTaskTwo (numbersArray) {
-  var newArrayOfNumbers = new Array();
-  for(var i=0; i<numbersArray.length; i++) {
-    if(numbersArray[i].length) {
-      for(var j=0; j<numbersArray[i].length; j++) {
-        newArrayOfNumbers[newArrayOfNumbers.length] = numbersArray[i][j];
-      }
-    } else {
-      newArrayOfNumbers[newArrayOfNumbers.length] = numbersArray[i];
-    }
-  }
-  return newArrayOfNumbers;
-} 
+var joeGomez = new FootballPlayer ("Joe Gomez", 23,16,4);
+var andrewRobertson = new FootballPlayer ("Andrew Robertson", 20,10,1);
+var sadioMane = new FootballPlayer ("Sadio Mane", 18,8,0);
+var allisonBecker = new FootballPlayer ("Allison Becker", 22,12,2);
+var necoWilliams = new FootballPlayer ("Neco Williams", 21,7,0);
 
-console.log(repackTaskTwo(someData));
+console.log(joeGomez, andrewRobertson, sadioMane, allisonBecker, necoWilliams);
 
-/* TASK 3: 
 
-Based on array:
 
-var someData = [13, 45, 56, [32, 11], 27, [55, 92]];
+/* TASK 3:
 
-Create a function that should receive array, get both subarrays from parent array,
-and pass it into another function, which should then join those arrays.
-In the end result should be returned and stored in variable which contains first function. */
+Try to do the same thing as in the previous task, but when creating player objects try to do it dinammicaly (using loop).
+For that you'll need data in following format:
+
+var players = [["Player name", 27, 12, 4], ["Player name", 27, 12, 4], ["Player name", 27, 12, 4]];
+
+This array is just example, change data in this array with data of players you choose. */
 
 console.log("TASK 3:");
 
-someData = [13, 45, 56, [32, 11], 27, [55, 92]];
+var players = [["Joe Gomez", 23, 16, 4],["Andrew Robertson", 20, 10, 1],["Sadio Mane", 18, 8, 0],["Allison Becker", 22, 12, 2],["Neco Williams", 21, 7, 0]];
 
-var repackTaskThree = function(numberArray, joiningInnerArrays) {
-  var innerArrays = new Array();
-  for(var i=0; i<numberArray.length; i++) {
-    if(numberArray[i].length === false) {
-      continue;
-    } else {
-      innerArrays = joiningInnerArrays(innerArrays, numberArray[i]);
-    }
+function playerTaskThree (a) {
+  var playersArray = [];
+  for(var i = 0; i<a.length; i++) {
+    playersArray[playersArray.length] = new FootballPlayer (a[i][0],a[i][1],a[i][2],a[i][3]);
   }
-
-  return innerArrays;
+  return playersArray;
 }
 
-var creatingNewArray = function (a, b) {
-  for(var i=0; i<b.length; i++) {
-    a[a.length] = b[i];
-  }
-  return a;
-}
+console.log(playerTaskThree(players));
 
-console.log(repackTaskThree(someData, creatingNewArray));
 
 /* TASK 4:
 
-Based on object:
-
-var someData = {
-  name: "Peter",
-  lastName: "Dinklage",
-  status: "married"
-};
-
-Create a function that should check if there is name in object,
-if yes it should return another function which should remove name from the function,
-if no then it should return a function which would set a name propery to the object,
-  with the value passed once function is called. */
+Create a construction function that should contain properties for first number, second number, and method. 
+Then create a four instances of object from that constructor, each with different numbers, and different method. 
+First should have method for multiplying of its own numbers, second for dividing, 
+third for adding and fourth for subtracting. */
 
 console.log("TASK 4:");
 
-someData = {
-  name: "Peter",
-  lastName: "Dinklage",
-  status: "married"
-};
-
-function checkingName (a) {
-  if(a.hasOwnProperty("name")) {
-    return function () {
-      delete a.name;
-      console.log("The property name was deleted because it existed in the copied object.");
-    }
-  } else {
-    return function (name) {
-      a.name = name;
-      console.log("The property name didn't exist in copied object. New value of the new created property is "+a.name);
-    }
-  }
+var multiplying = function(a,b) {
+  return a*b;
 }
 
-checkingName(someData)("John");
-checkingName(someData)("John");
+var dividing = function(a,b) {
+  return a/b;
+}
+
+var adding = function(a,b) {
+  return a+b;
+}
+
+var subtracting = function(a,b) {
+  return a-b;
+}
+
+function Calculator(firstNumber, secondNumber, operation) {
+  this.firstNumber = firstNumber;
+  this.secondNumber = secondNumber;
+  this.operation = operation(this.firstNumber, this.secondNumber);
+}
+
+var exampleOne = new Calculator(5,5,multiplying);
+console.log(exampleOne);
+
+var exampleTwo = new Calculator(36,9,dividing);
+console.log(exampleTwo);
+
+var exampleThree = new Calculator(4.1,3.9,adding);
+console.log(exampleThree);
+
+var exampleFour = new Calculator(10,3,subtracting);
+console.log(exampleFour);
 
 /* TASK 5:
 
-Based on object:
+Test your understanding of closures and scope with following examples.
 
-var someData = {
-  name: "Peter",
-  lastName: "Dinklage",
-  status: "married"
-};
+IMPORTANT! - First read the task and try to give an answer,
+and then copy the code to your file and check what do you get in console.
 
-Create a method that should check if there is name in object,
-if yes it should create second method in the same object and then call it.
-The second method should remove name from the function, and console log the object in it's current state. 
-The second method should then create a third method and call it. 
-Third method should add name property back to the object, with value of "Mike", and then console log object.
 
-BONUS:
+    QUESTION 1:
 
-Try to set name with value it had before. */
+  What’s the result of executing this code and why?
 
-console.log("TASK 5:");
 
-someData = {
-  name: "Peter",
-  lastName: "Dinklage",
-  status: "married"
-};
+    ANSWER 1:
+  --- the result is: 
 
-someData.checkingPropName = function(newName) {
-  if(this.hasOwnProperty("name")) {
-    var resetingName = newName;
-    this.removingPropName = function (newName) {
-      var firstName = newName;
-      delete this.name;
-      console.log(this);
-      /* Vladice dodala sam sledecu liniju da console.log-uje samo ime,
-      jer mi nije jasno sto u prethodnoj liniji izbacuje i name u propertijima(kad se otvori objekat sa strelicom u konzoli).
-      Ovako radi - izbacuje undefined - videces kad pokrenes. Trazila sam na netu,
-      ali se nisam nesto snasla */
-      console.log("Name in current state: "+this.name);
-      this.addingNewName = function (newName) {
-        this.name = newName;
-        console.log(this);
-        console.log("Name in current state: "+this.name);
+  undefined 
+  2
+
+  because var a is called in console.log before declaration, 
+  and the number two will be visible in console.log because function foo() is defined as function declaration.
+  All that happened because of creation faze of hoisting.
+
+
+    QUESTION 2:
+
+  What is result?
+
+    ANSWER 2:
+  --- the result is: 
+
+  nothing, 
+  if we console.log firstResult and result:
+
+      function otherFunction(input) {
+        return a;
       }
-      this.addingNewName(firstName);
-    }
-    this.removingPropName(resetingName);
+      5
 
-  } else {
-    console.log(this);
+
+    QUESTION 3:
+
+  What is the result of the following code? Explain your answer.
+
+    ANSWER 3:
+  --- the result is: 
+
+  Aurelio De Rosa       -  because Aurelio De Rosa is scope for fullname in obj.properties
+  function() {
+    return this.fullname;
   }
-}
-console.log("Checking with name Mike");
-someData.checkingPropName("Mike");
-
-console.log("Checking with name Peter");
-someData.checkingPropName("Peter");
+  John Doe              -  because John Doe is scope for fullname in test();
 
 
-/* ***** JS PRACTISE HOMEWORK *****
+    QUESTION 4:
 
-TASK 1:
+  What will you see in the console for the following example?
 
-Create array with data, and loop through it, so in the end you console log this:
+    ANSWER 4:
+  --- the result is:
+    
+    1
+*/
 
-"At position 0, subposition 0, value is 1"
-"At position 0, subposition 1, value is 2"
-"At position 0, subposition 2, value is 3"
-"At position 1, subposition 0, value is 4"
-"At position 1, subposition 1, value is 5"
-"At position 1, subposition 2, value is 6"
-"At position 2, subposition 0, value is 7"
-"At position 2, subposition 1, value is 8"
-"At position 2, subposition 2, value is 9" */
-
-console.log("HOMEWORK TASK 1:");
-
-var data = [[1,2,3],[4,5,6],[7,8,9]];
-
-data.printData = function() {
-  for(var i=0; i<this.length; i++) {
-    for(var j=0; j<this[i].length; j++) {
-      console.log("At position "+ i +", subposition "+ j +", value is "+this[i][j]);
-    }
-  }
-}
-
-data.printData();
-
-
-/* TASK 2: 
-
-Create a method that should console log name and age from any object.
-Then create array with multiple objects, each object should contain different names and ages.
-Call a method on each of them. */
-
-console.log("HOMEWORK TASK 2:");
-
-Object.prototype.printNameAndAge = function () {
-  console.log("Name of this person is "+this.name+" and his age is "+this.age);
-}
-
-var persons = [
-{
-  name: "Mike",
-  age: 44
-},
-{
-  name: "John",
-  age: 16
-},
-{
-  name: "Ian",
-  age: 28
-},
-{
-  name: "Jim",
-  age: 30
-},
-{
-  name: "Dean",
-  age: 15
-}
-];
-
-function loopPersons (a) {
-  for(var i=0; i<a.length; i++) {
-    a[i].printNameAndAge();
-  }
-}
-
-loopPersons(persons);
-
-var someExample = {
-  address: "Belgrade",
-  phoneNumber: 123456789,
-  age: 55
-}
-
-console.log("TEST");
-someExample.printNameAndAge();
