e10d3bf76be0f70fc2463e685173c97f58210a5c
Elena Milosevic <elenagiura94@gmail.com>
2023-12-29 21:54:30 +0100

    Changing tasks numbering

--- js/homework.js
+++ js/homework.js
@@ -1,4 +1,4 @@
-/* TASK 1: 
+/* TASK 4: 
 Write a JavaScript conditional statement to find the largest of five numbers.
 Console log the result in the string format (sentence + number variable).
 Numbers : 15, -2, 22, 0, 13 
@@ -10,19 +10,19 @@ var c = 22;
 var d = 0;
 var e = 13;
 
-if (a>b && a>c && a>d && a>e) {
-	console.log ("Largest number is: "+ a);
-} else if (b>a && b>c && b>d && b>e) {
-	console.log ("Largest number is: "+ b);
-} else if (c>a && c>b && c>d && c>e) {
-	console.log ("Largest number is: "+ c);
-} else if (d>a && d>c && d>b && d>e) {
-	console.log ("Largest number is: "+ d);
+if (a > b && a > c && a > d && a > e) {
+  console.log('Largest number is: ' + a);
+} else if (b > a && b > c && b > d && b > e) {
+  console.log('Largest number is: ' + b);
+} else if (c > a && c > b && c > d && c > e) {
+  console.log('Largest number is: ' + c);
+} else if (d > a && d > c && d > b && d > e) {
+  console.log('Largest number is: ' + d);
 } else {
-	console.log ("Largest number is: "+ e);
-} 
+  console.log('Largest number is: ' + e);
+}
 
-/* TASK 2: 
+/* TASk 5: 
 Write a JavaScript conditional statement to find the sign (+ or -) of product of three numbers. Console log the result.
 Numbers : 3, -7, 2 
 */
@@ -31,42 +31,38 @@ a = 3;
 b = -7;
 c = 2;
 
-if (a*b*c>=0) {
-	console.log("Sign is +");
+if (a * b * c >= 0) {
+  console.log('Sign is +');
 } else {
-	console.log("Sign is -");
+  console.log('Sign is -');
 }
 
-
-/* TASK 3: 
+/* TASK 6: 
 Write a switch statement to console log "Hello world" in 5 different languages
 */
-a = "Russian";
+a = 'Russian';
 
 switch (a) {
-	case "English":
-		console.log("Hello World");
-	break;
-	case "Serbian":
-		console.log("Zdravo svete");
-	break;
-	case "Romanian":
-		console.log("Salut Lume");
-	break;
-	case "French":
-		console.log("Bonjour le monde");
-	break;
-	case "Slovak":
-		console.log("Ahoj svet");
-	break;
-	default:
-		console.log("nothing");
+  case 'English':
+    console.log('Hello World');
+    break;
+  case 'Serbian':
+    console.log('Zdravo svete');
+    break;
+  case 'Romanian':
+    console.log('Salut Lume');
+    break;
+  case 'French':
+    console.log('Bonjour le monde');
+    break;
+  case 'Slovak':
+    console.log('Ahoj svet');
+    break;
+  default:
+    console.log('nothing');
 }
 
-
-
-
-/* TASK 4: 
+/* TASK 7: 
 Create userAge variable, and give it some value.
 Write a ternary operator that checks if a user is 28 or older. Store result in a variable.
 Then write an if/else statement using result from variable. 
@@ -82,14 +78,14 @@ If not, console log:
 var userAge = 26;
 var result;
 
-(userAge>=28 ? result=true : result=false)
+userAge >= 28 ? (result = true) : (result = false);
 
-if(result===false) {
-	console.log("User is less than 28 years old");
+if (result === false) {
+  console.log('User is less than 28 years old');
 } else {
-	if(userAge>28) {
-		console.log("User is older than 28");
-	} else {
-		console.log("User is 28");
-	}
+  if (userAge > 28) {
+    console.log('User is older than 28');
+  } else {
+    console.log('User is 28');
+  }
 }
